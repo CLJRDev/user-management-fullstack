e1bc17cf5856bee9c45f04c6f532258fb5ebcfc1
CLJR <nguyenconglam2002a@gmail.com>
2024-03-15 01:22:08 +0700

    Implementing Signup in React

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,9 +1,35 @@
 import { Link } from "react-router-dom"
+import { useRef } from "react";
+import axiosClient from "../axios-client";
 
 export default function Signup(){
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmationRef = useRef();
+
+  const {setUser, setToken} = useStateContext();
 
   const onSubmit = (event) => {
-    event.preventDeafult()
+    event.preventDefault()
+    const payload = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+      password_confirmation: passwordConfirmationRef.current.value
+    }
+    
+    axiosClient().post('/signup', payload)
+      .then(({data}) => {
+        setUser(data.user)
+        setToken(data.token)
+      })
+      .catch(err => {
+        const response = err.response;
+        if(response && response.status === 422){
+          console.log(response.data.errors);
+        }
+      })
   }
 
   return (
@@ -13,10 +39,10 @@ export default function Signup(){
           <h1 className="title">
             Signup for free
           </h1>
-          <input type='text' placeholder="Full Name" />
-          <input type='email' placeholder="Email Address" />
-          <input type='password' placeholder="Password" />
-          <input type='password' placeholder="Password Confirmation" />
+          <input ref={nameRef} type='text' placeholder="Full Name" />
+          <input ref={emailRef} type='email' placeholder="Email Address" />
+          <input ref={passwordRef} type='password' placeholder="Password" />
+          <input ref={passwordConfirmationRef} type='password' placeholder="Password Confirmation" />
           <button className="btn btn-block">Signup</button>
           <p className="message">
             Already Registered? <Link to='/login'>Sign in</Link>
